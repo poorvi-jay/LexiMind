77b6f33f908b08e644186272ec422251bc2b6e32
pragathi_jp <pragathijpastay18@gmail.com>
2026-08-28 15:44:13 +0530

    chore: add check_tables.py dev utility for inspecting SQLite schema

---
+++ check_tables.py
@@ -0,0 +1,9 @@
+import sqlite3
+conn = sqlite3.connect('backend/dev.db')
+cursor = conn.cursor()
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+tables = cursor.fetchall()
+print('Tables in database:')
+for t in tables:
+    print(' -', t[0])
+conn.close()
